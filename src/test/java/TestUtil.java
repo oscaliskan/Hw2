bd63180eb46e475800bd8c526c08b9179a8fa6d9
oscaliskan <oscaliskan@gmail.com>
2019-03-06 03:46:52 +0300

    Util Tests 001

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,4 +14,22 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+  public void testLengthZero() {
+    int[] arr = {0};
+    assertEquals(false,c.compute(arr));
+  }
+  
+  @Test
+  public void testLengthEven() {
+    int[] arr = {0,1};
+    assertEquals(false,c.compute(arr));
+  }
+
+  @Test
+  public void testSum() {
+    int[] arr = {3,5,7,11,13};
+    assertTrue(c.compute(arr));
+  }
 }
